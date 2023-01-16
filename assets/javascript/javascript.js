819302fb0fb86c33441de944ac860e211cc1857c
Hamza-business <hamzadev1business@gmail.com>
2023-01-16 13:57:08 +0300

    courses page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -35,3 +35,102 @@ function percent(){
         }
     }
 }
+
+
+function showLects(){
+    document.querySelector('.column.is-2').classList.toggle('hidelectS')
+}
+
+function lecturesDo(){
+    let lectures = [...document.querySelectorAll(".lecter")];
+    for(let i=0; i<lectures.length; i++){
+        lectures[i].textContent = i+1;
+        lectures[i].addEventListener('click',()=>{
+            lectures.forEach(ele=>{
+                ele.classList.remove("current");
+            })
+            lectures[i].classList.add("current")
+        })
+    }
+}
+
+
+
+// Search Functionality
+let TileFilter = "";
+let yearFilter = "";
+let stageFilter = "";
+
+function setTtile(val){
+    TileFilter = val;
+    searchFilter();
+}
+function setYear(val){
+    yearFilter = val;
+    searchFilter();
+}
+function setStage(val){
+    stageFilter = val;
+    searchFilter();
+}
+function getAllcourses(clNm){
+    let allCourses = document.querySelectorAll(`.${clNm}`)
+    return [...allCourses]
+}
+
+function searchFilter(){
+    let courses = getAllcourses("card");
+    courses.forEach((ele)=>{
+        ele.classList.remove("filtered")
+        if(ele.querySelector(".title").textContent.includes(TileFilter)){
+            if(ele.querySelector(".year").textContent.includes(yearFilter)){
+                if(ele.querySelector(".stage").textContent.includes(stageFilter)){
+                    ele.classList.add("filtered")
+                }
+            }
+        }
+    })
+    makeCardPages();
+}
+
+function makeCardPages(){
+    let cards = getAllcourses("filtered");
+    let cardNum = 8;
+    let pagNmbr = Math.ceil(cards.length / cardNum);
+    let pcparent = document.querySelector(".pagesCount");
+    pcparent.innerHTML = "";
+
+    for(let i=0; i<pagNmbr; i++){
+        let newP = document.createElement("p")
+        newP.textContent = i+1;
+
+        let start = i*cardNum;
+        let end = ((i+1)*cardNum) - 1;
+
+        newP.addEventListener('click',()=>{
+            let allP = [...document.querySelectorAll(".pagesCount p")];
+            allP.forEach((ele)=>{
+                ele.classList.remove("active");
+            });
+
+            newP.classList.add("active");
+            
+            cards.forEach((card,i)=>{
+                if(start <= i && i <= end){
+                    card.classList.add("active");
+                }
+                else card.classList.remove("active");
+            })
+        })
+
+        pcparent.append(newP)
+    }
+
+    try{
+        pcparent.firstElementChild.click();
+        document.querySelector(".ifnot").style.display = "none";
+    }catch(e){
+        document.querySelector(".ifnot").style.display = "block";
+    }
+    
+}
